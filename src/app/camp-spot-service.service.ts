3027a2302412d08c2c4f2dd4f4345976c189e07c
Steven Kauyedauty <skauyedauty@gmail.com>
2017-01-26 09:41:51 -0700

    mapped objects into reservations

--- src/app/camp-spot-service.service.ts
+++ src/app/camp-spot-service.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/merge';
+
 
 
 
@@ -16,23 +18,26 @@ export class CampSpotService {
   private _campInfoUrl = 'api/campInfo/campInfo.json';
   constructor(private _http: Http) { }
 
-  getAvailableCamps() {  // Observable<IProduct>
+  getAvailableCamps() {
     return this._http.get(this._campInfoUrl)
       .map((response: Response) => response.json()) // <IProduct[]>
-      .map(data => {
-        return { ...data.reservations, ...data.search};
-      })
-      // .filter((tripDate, index ) => {
-      //   console.log(tripDate, index, 'this is filter')
-      //  return tripDate.search ? tripDate.search : false;
-      //  // return tripDate;
-      //   // return  tripDate === 'search' || tripDate === 'search';
-      // })
-      //.map(dates => ({dateList:dates.reservations}))
-      //.map(dates => ({dateList: Object.assign({}, dates.res, dates.reservations )}))      
+      .map((tripDate) => ({
+        reservations: tripDate.reservations
+          .map(reservation => Object.assign(
+            {},
+            reservation,
+            {
+              searchStartDate: tripDate.search.startDate,
+              searchEndDate: tripDate.search.endDate
+            }
+          )
+        )
+      }))
+      //.map((data,index) => (data.reservations[0]))
       .do(data => console.log('all: ' + JSON.stringify([data])))
       .catch(this.handleError);
   }
+
 
   private handleError(error: Response) {
     console.log(error);
@@ -40,13 +45,7 @@ export class CampSpotService {
   }
 
 }
+            //.map(data => [data.search].concat(data.reservations)
+                  //.map(dates => ({dateList: Object.assign({}, dates.reservations, dates.newRes)}))
 
       //.filter( item => moment(item.resStart).isBefore(item.reservations) )
-
-
-// .map(item => (
-      //   {
-      //     tripLength: moment(item.search.endDate).diff(item.search.startDate, 'days'),
-      //     gap: item.gapRules,
-      //     reservations: item.reservations
-      //   }) )
